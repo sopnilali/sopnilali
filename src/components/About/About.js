64c55ed2140638d97713be48be9b6fab06b282ae
amiadud <sopnilstar@gmail.com>
2023-12-12 15:36:04 +0600

    experience update

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -9,12 +9,12 @@ const About = () => {
         <div className="w-full min-h-screen py-20 mx-auto bg-slate-900/60 backdrop-blur-sm" >
             <SectionTitle heading={'About'}/>
             <div className="divider "></div>
-        <div className='w-1/3 mx-auto mt-14'>
+        <div className='md:w-3/4 w-3/4  mx-auto mt-10'>
         <h2 className='text-justify text-2xl text-white'>
         I'am Md. Abdul Adud. I am from Rajshahi, Bangladesh. I have completed my Bsc graduation in CSE from Varendra University.
         </h2>
-        <h2 className='text-justify text-2xl text-white my-20'>
-        I have been very passionate about computers, programming since my university life. I have 2+ years of experience in Web Development as a back-end and a front-end developer. Knowledge of NodeJS, ReactJS, and MongoDB.
+        <h2 className='text-justify text-2xl text-white mt-10'>
+        I have been very passionate about computers, programming since my university life. I have 2 month of experience in Web Development as a front-end developer. Knowledge of NodeJS, ReactJS, and MongoDB.
         </h2>
         </div>
 
